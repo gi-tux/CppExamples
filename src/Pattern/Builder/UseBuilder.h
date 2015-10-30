9f5528975ffd80e5567f0c1bbfa7682d0a41c27f
Kelly Fulks <kelly@family.fulks.org>
2015-10-30 18:30:18 -0500

    Fixes #17 - Added an example of a builder design pattern.

---
+++ src/Pattern/Builder/UseBuilder.h
@@ -0,0 +1,15 @@
+/*
+ * UseBuilder.h
+ *
+ *  Created on: Oct 29, 2015
+ *      Author: kelly
+ */
+
+#ifndef SRC_PATTERN_BUILDER_USEBUILDER_H_
+#define SRC_PATTERN_BUILDER_USEBUILDER_H_
+
+#include "ShapeBuilder.h"
+#include "RectangleBuilder.h"
+#include "TriangleBuilder.h"
+
+#endif /* SRC_PATTERN_BUILDER_USEBUILDER_H_ */
